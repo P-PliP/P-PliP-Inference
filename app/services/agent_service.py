a8b61b6020122db19a5bd1ee7e6af76f9b6e0165
smj1513 <minj21@naver.com>
2025-12-12 23:02:31 +0900

    feat: 관광지 추천 프롬프트 개선

--- app/services/agent_service.py
+++ app/services/agent_service.py
@@ -4,7 +4,9 @@ from app.db.filters import build_geo_fileter, build_geo_fileter_with_content_typ
 from app.api.v1.response.ai_response import SuggestResponse
 from typing import List
 from langchain.schema import Document
+from langchain.retrievers import MultiQueryRetriever
 from app.agents.graph import rerank_chain
+from app.core.llm import mini_llm
 
 
 class AgentService:
@@ -24,11 +26,27 @@ class AgentService:
         filter = build_geo_fileter_with_content_type(
             request.lat, request.lng, request.m, request.content_type
         )
-
         retriever = get_ensemble_retriever(
             k=100, dense_weight=0.7, sparse_weight=0.3, filter=filter
         )
+        # retriever = MultiQueryRetriever.from_llm(
+        #     retriever=get_ensemble_retriever(
+        #         k=100, dense_weight=0.7, sparse_weight=0.3, filter=filter
+        #     ),
+        #     llm=mini_llm,
+        # )
         docs = await retriever.ainvoke(request.query)
+
+        # 1. 중복 제거 루틴 추가
+        # EnsembleRetriever 특성상, 같은 문서라도 점수가 다르면 중복으로 잡힐 수 있습니다.
+        # 따라서 `no`를 기준으로 중복을 제거합니다.
+        unique_docs = {}
+        for doc in docs:
+            doc_id = doc.metadata.get("no")
+            if doc_id not in unique_docs:
+                unique_docs[doc_id] = doc
+        docs = list(unique_docs.values())
+
         if len(docs) > 100:
             docs = docs[:100]
         docs = await self.rerank_documents(
@@ -57,7 +75,7 @@ class AgentService:
         self, query: str, retrieved_docs: List[Document], top_k: int
     ) -> List[Document]:
         # (1) 배치 사이즈 설정 (한 번에 10개씩 평가)
-        batch_size = 5
+        batch_size = 10
         batches = [
             retrieved_docs[i : i + batch_size]
             for i in range(0, len(retrieved_docs), batch_size)
@@ -104,7 +122,8 @@ class AgentService:
             original_doc = retrieved_docs[item]
             # 메타데이터에 점수 추가 (선택사항)
             original_doc.metadata["relevance_score"] = unique_scores[item]
-            final_docs.append(original_doc)
+            if unique_scores[item] > 0:
+                final_docs.append(original_doc)
             print(f"- [Score: {unique_scores[item]}] ID: {item}")
 
         return final_docs
